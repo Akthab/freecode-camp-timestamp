25b531eece657c90b88161ec5fa3cead20d2309d
Akthab <mhmdakthab6@gmail.com>
2023-11-15 13:55:36 +0530

    feat():  added endpoint to get unixtmp / utcString

--- index.js
+++ index.js
@@ -6,27 +6,45 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+	res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/:date?', (req, res) => {
+	const dateStr = req.params.date;
+	let date;
 
+	if (!dateStr) {
+		date = new Date();
+	} else {
+		try {
+			date = new Date(dateStr);
+		} catch (error) {
+			res.json({ error: 'Invalid Date' });
+			return;
+		}
+	}
+
+	const unixTimestamp = date.getTime();
+	const utcString = date.toUTCString();
+
+	res.json({ unix: unixTimestamp, utc: utcString });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+	console.log('Your app is listening on port ' + listener.address().port);
 });
